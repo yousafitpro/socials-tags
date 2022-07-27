881fcc22a2c960b8aa105e2fdc4fd5c4c70f2715
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2022-07-27 21:29:57 +0500

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,9 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{url('privacy-policy')}}">Privacy Policy</a>
+                            </li>
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
