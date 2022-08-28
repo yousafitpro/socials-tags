10266221f26731ca7f8c4180c481e9cd4b9e87e0
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2022-08-28 12:13:34 +0500

    ok

--- resources/views/daily/daily.blade.php
+++ resources/views/daily/daily.blade.php
@@ -45,6 +45,9 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script>
+    if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+       alert("Let's get this party started")
+    }
     function getLocalStream() {
         navigator.mediaDevices.getUserMedia({video: true, audio: true}).then( stream => {
             console.log("stream",stream)
