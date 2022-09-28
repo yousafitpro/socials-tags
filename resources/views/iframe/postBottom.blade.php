c97b7d8feb4b2a6ee5025e2cf76c849eb45c6232
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-09-28 07:05:05 -0700

    ok

--- resources/views/iframe/postBottom.blade.php
+++ resources/views/iframe/postBottom.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
     @if($type=="image")
-        <div class="col-3">
+        <div class="col-2">
             <img src="https://www.placecage.com/640/360" style="width: 40px; height: 40px; border-radius: 50%">
         </div>
         @endif
-    <div class="col-{{$type=='text'?'9':'6'}}">
+    <div class="col-{{$type=='text'?'9':'7'}}">
         <label style="font-size: 13px; color: gray; font-weight: bold">Prof BlackTruth</label><div></div>
         <small>6 days ago</small>
     </div>
