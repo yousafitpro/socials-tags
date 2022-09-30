da3291b3e0121566f9a9248cb8ae697714eed870
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-09-30 06:06:39 -0700

    overflow handled

--- resources/views/iframe/postPopup.blade.php
+++ resources/views/iframe/postPopup.blade.php
@@ -81,7 +81,7 @@
                                 </div>
                             </div>
                             @endif
-                        <div style="position: absolute;bottom: 10vh; right: 100;">
+                        <div style="position: absolute;bottom: 6vh; right: 100;">
 
                             <img src="{{asset('icon/fb.png')}}" style="width: 30px; border-radius: 5px">
                             <img src="{{asset('icon/tw.png')}}" style="width: 30px; border-radius: 5px">
