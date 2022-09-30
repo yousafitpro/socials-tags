c2009304dcb24bcab9fe5c317cecea5198de1990
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-09-30 10:14:05 -0700

    overflow handled

--- resources/views/iframe/js.blade.php
+++ resources/views/iframe/js.blade.php
@@ -10,7 +10,7 @@
     function fullScreen(id)
     {
         // document.querySelector(id).requestFullscreen();
-        window.open('https://votersnews.com/wall/iframe','_blank')
+        window.open('https://votersnews.com/wall/iframe')
     }
     function exitFullScreen()
     {
