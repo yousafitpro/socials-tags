8cf539ae677035a66e954c9ef1434c37e2bc7f67
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-09-29 22:34:45 -0700

    our wall added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,11 +50,11 @@
                             @endif
 
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+{{--                            @if (Route::has('register'))--}}
+{{--                                <li class="nav-item">--}}
+{{--                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
+{{--                                </li>--}}
+{{--                            @endif--}}
                         @else
                             <?php
                             if (auth()->check())
