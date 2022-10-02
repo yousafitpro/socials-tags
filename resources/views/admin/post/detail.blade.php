2d640cbfd842382e8e62462564bc42f96ff30da0
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-10-02 00:40:58 -0700

    ok

--- resources/views/admin/post/detail.blade.php
+++ resources/views/admin/post/detail.blade.php
@@ -25,8 +25,24 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="p-2">
-                                    @include('iframe.postBottom',['type'=>$post->type,'username'=>$post->username,'p'=>$post,'category'=>$post])
-                                </div>
+                                    <div class="row">
+                                        @if($type=="image")
+                                            <div class="col-1">
+                                                <img src="https://www.placecage.com/640/360" style="width: 40px; height: 40px; border-radius: 50%">
+                                            </div>
+                                        @endif
+                                        <div class="col-{{$type=='text'?'9':'7'}}">
+                                            <label style="font-size: 13px; color: gray; font-weight: bold">{{$username}}</label><div></div>
+                                            <small>{{ \Carbon\Carbon::parse($p->created_at)->diffForhumans() }}</small>
+                                        </div>
+                                        <div class="col-4">
+                                            @if($category=="youtube")
+                                                <img src="{{asset('wall/youtube.png')}}">
+                                            @elseif($category=="native")
+                                                <img src="{{asset('wall/comment.png')}}">
+                                            @endif
+                                        </div>
+                                    </div>                                </div>
                             </div>
                         </div>
                         <div style="padding: 10px">
