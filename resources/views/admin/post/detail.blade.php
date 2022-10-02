7c8dad07325c1f9354cfd140dbc2a34408318a62
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-10-02 00:42:21 -0700

    ok

--- resources/views/admin/post/detail.blade.php
+++ resources/views/admin/post/detail.blade.php
@@ -23,7 +23,7 @@
 
 <br>
                         <div class="row">
-                            <div class="col-md-12">
+                            <div class="col-md-4 offset-4">
                                 <div class="p-2">
                                     <div class="row">
                                         @if($post->type=="image")
