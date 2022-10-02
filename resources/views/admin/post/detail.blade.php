7fe8c71b9de8c90604ccad76f0ad4a05ae72e7f0
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-10-02 00:41:44 -0700

    ok

--- resources/views/admin/post/detail.blade.php
+++ resources/views/admin/post/detail.blade.php
@@ -26,19 +26,19 @@
                             <div class="col-md-12">
                                 <div class="p-2">
                                     <div class="row">
-                                        @if($type=="image")
+                                        @if($post->type=="image")
                                             <div class="col-1">
                                                 <img src="https://www.placecage.com/640/360" style="width: 40px; height: 40px; border-radius: 50%">
                                             </div>
                                         @endif
-                                        <div class="col-{{$type=='text'?'9':'7'}}">
-                                            <label style="font-size: 13px; color: gray; font-weight: bold">{{$username}}</label><div></div>
-                                            <small>{{ \Carbon\Carbon::parse($p->created_at)->diffForhumans() }}</small>
+                                        <div class="col-{{$post->type=='text'?'9':'7'}}">
+                                            <label style="font-size: 13px; color: gray; font-weight: bold">{{$post->username}}</label><div></div>
+                                            <small>{{ \Carbon\Carbon::parse($post->created_at)->diffForhumans() }}</small>
                                         </div>
                                         <div class="col-4">
-                                            @if($category=="youtube")
+                                            @if($post->category=="youtube")
                                                 <img src="{{asset('wall/youtube.png')}}">
-                                            @elseif($category=="native")
+                                            @elseif($post->category=="native")
                                                 <img src="{{asset('wall/comment.png')}}">
                                             @endif
                                         </div>
