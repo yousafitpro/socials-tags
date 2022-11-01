39614a29aab2a92b139508a2b5e63c4154f76ed4
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 11:20:39 -0700

    ok

--- app/Http/Controllers/socialConnectController.php
+++ app/Http/Controllers/socialConnectController.php
@@ -15,7 +15,7 @@ class socialConnectController extends Controller
     public function socialConnections()
     {
        $user=User::where('email','yousaf.itpro@gmail.com')->first();
-       dd($user->roles());
+       dd($user->roles->pluck('name'));
         return view('social.socialConnections');
     }
 }
