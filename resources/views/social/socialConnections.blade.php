9363718ce52c9a6e618296db2550783713b437d0
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 11:34:53 -0700

    ok

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -1,3 +1,11 @@
 @extends('layouts.dashboard')
 @section('content')
+
+    <h3>Social Connections</h3>
+    <br>
+    <div class="card">
+        <div class="card-body">
+            aasdasdasdasdasd
+        </div>
+    </div>
 @endsection
