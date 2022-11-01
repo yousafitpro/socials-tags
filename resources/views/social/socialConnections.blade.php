03ce9ebaefe8c0a4e91d87ae5012395094b940cf
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 12:01:58 -0700

    ok

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -74,7 +74,7 @@
                     console.log("Success",response)
                     $.ajax({
                         url:"{{url('social-connect/save-facebook-token')}}",
-                        method:'get',
+                        method:'post',
                         data: {"_token": "{{ csrf_token() }}",'access_token':response.authResponse.accessToken,'userid':response.authResponse.userID,'expire_in':response.authResponse.expiresIn,'expire_at':response.authResponse.expiresIn,'expire_at':response.authResponse.data_access_expiration_time},
                         beforeSend:function(){
 
