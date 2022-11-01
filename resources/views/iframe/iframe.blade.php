6cdd36e6c33b0d16d61c9fc6ee4741a1985e9c39
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 09:19:25 -0700

    ok

--- resources/views/iframe/iframe.blade.php
+++ resources/views/iframe/iframe.blade.php
@@ -31,7 +31,7 @@
                             text:text,
                             url:url
                         }).then(function (data){
-                            alert("Success! Successfully Shared")
+                            // alert("Success! Successfully Shared")
                         }).catch(function (error){
                             alert("Sorry! Cannot be shared")
                         })
