1145534cfbf2a115bcfcfdd7dae61f36b340d668
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 11:31:06 -0700

    ok

--- app/Http/Controllers/socialConnectController.php
+++ app/Http/Controllers/socialConnectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\socialconnection;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,13 @@ class socialConnectController extends Controller
     }
     public function socialConnections()
     {
-
+           $c=new socialconnection();
+           $c->name="Facebook";
+           $c->save();
+        $c=new socialconnection();
+        $c->name="Instagram";
+        $c->save();
+        dd(socialconnection::all());
         return view('social.socialConnections');
     }
 }
