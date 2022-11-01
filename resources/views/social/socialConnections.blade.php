198ebb0a4688995e7e2ef4a75547ea207e8b0772
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 11:35:51 -0700

    ok

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.dashboard')
 @section('content')
 
-    <h3>Social Connections</h3>
-    <br>
-    <div class="card">
-        <div class="card-body">
-            aasdasdasdasdasd
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <h3 class="text-center">Social Connections</h3>
+            <br>
+            <div class="card">
+                <div class="card-body">
+                    aasdasdasdasdasd
+                </div>
+            </div>
         </div>
     </div>
+</div>
 @endsection
