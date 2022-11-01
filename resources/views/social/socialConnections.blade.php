55d6284a82abe7f08f70ad349eb50985edfdb504
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 11:40:19 -0700

    ok

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -6,17 +6,28 @@
         <div class="col-md-6 offset-md-3">
             <h3 class="text-center">Social Connections</h3>
             <br>
+            <?php
+                $connections=\App\Models\socialconnection::where('user_id',auth()->user()->id)->get();
+                ?>
             <div class="card">
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-3">
+                    @foreach($connections as $c)
+                        <div class="row">
+                            <div class="col-md-3">
+                            </div>
+                            <div class="col-md-6">
+                                <label></label>
+                            </div>
+                            <div class="col-md-3">
+                                @if($c->status=="Disconnected")
+                                    <button class="btn btn-outline-primary form-control">Connect</button>
+                                    @endif
+                                    @if($c->status!="Disconnected")
+                                        <button class="btn btn-outline-success form-control">Connected</button>
+                                    @endif
+                            </div>
                         </div>
-                        <div class="col-md-6">
-                        </div>
-                        <div class="col-md-3">
-                            <button class="btn btn-outline-primary form-control">Connect</button>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
