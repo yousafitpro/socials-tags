f1cc748ee20ee9dd4def9580e9ef41303157b298
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 10:43:59 -0700

    ok

---
+++ resources/views/social/facebook-login.blade.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>Title of the document</title>
+</head>
+
+<body>
+<button onclick="login()">Login</button>
+</body>
+
+</html>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '{your-app-id}',
+            cookie     : true,
+            xfbml      : true,
+            version    : '{api-version}'
+        });
+
+        FB.AppEvents.logPageView();
+
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+function login()
+{
+    alert("ok")
+    FB.login()
+}
+</script>
