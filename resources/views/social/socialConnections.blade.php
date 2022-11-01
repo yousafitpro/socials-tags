de095de82900f9712f35481b99a1a214006085f5
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 11:51:50 -0700

    ok

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -22,7 +22,7 @@
                             </div>
                             <div class="col-md-3">
                                 @if($c->status=="Disconnected")
-                                    <button class="btn btn-outline-primary form-control">Connect</button>
+                                    <button class="btn btn-outline-primary form-control" onclick="connect('{{$c->name}}')">Connect</button>
                                     @endif
                                     @if($c->status!="Disconnected")
                                         <button class="btn btn-outline-success form-control">Connected</button>
@@ -37,4 +37,47 @@
         </div>
     </div>
 </div>
+    <script>
+
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '1406523236791020',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v15.0'
+            });
+
+            FB.AppEvents.logPageView();
+
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+        function connect(name)
+        {
+            if(name=="Facebook")
+            {
+                facebookLogin();
+            }
+        }
+        function facebookLogin()
+        {
+
+            FB.login(function(response) {
+                console.log("data",response)
+                if (response.authResponse) {
+                    console.log("Success",response)
+                    // The person logged into your app
+                } else {
+                    console.log("error".response)
+                    // The person cancelled the login dialog
+                }
+            });
+        }
+    </script>
 @endsection
