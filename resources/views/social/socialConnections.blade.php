2add300df4856f442f2d19f50d29bdae143b511a
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-01 11:37:44 -0700

    ok

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -3,12 +3,20 @@
 
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-4 offset-md-4">
+        <div class="col-md-6 offset-md-3">
             <h3 class="text-center">Social Connections</h3>
             <br>
             <div class="card">
                 <div class="card-body">
-                    aasdasdasdasdasd
+                    <div class="row">
+                        <div class="col-md-3">
+                        </div>
+                        <div class="col-md-6">
+                        </div>
+                        <div class="col-md-3">
+                            <button class="btn btn-outline-primary">Connect</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
