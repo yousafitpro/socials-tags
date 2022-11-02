31ffd880a114533a0e8bef2c2c7226fe98133ebe
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-02 11:52:01 -0700

    ok

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -41,7 +41,7 @@
 
         window.fbAsyncInit = function() {
             FB.init({
-                appId      : '1406523236791020',
+                appId      : '1957610567771338',
                 cookie     : true,
                 xfbml      : true,
                 version    : 'v15.0'
