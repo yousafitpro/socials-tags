7970ac3e8801d6fd86324162b0e85a5dca8b1ec2
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-02 12:29:36 -0700

    ok

--- app/Http/Controllers/socialConnectController.php
+++ app/Http/Controllers/socialConnectController.php
@@ -30,7 +30,7 @@ class socialConnectController extends Controller
     }
     public function disconnect_connection($id)
     {
-        //asdasd
+        //asdasdasdsad
         $c=socialconnection::find($id);
         $c->status="Disconnected";
         $c->save();
