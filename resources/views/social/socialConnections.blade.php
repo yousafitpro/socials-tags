92448de5d117215f3014da436a75dda2dbedf65d
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-11-14 04:28:36 -0800

    social connect

--- resources/views/social/socialConnections.blade.php
+++ resources/views/social/socialConnections.blade.php
@@ -41,29 +41,29 @@
 </div>
 @if(request('state') && request('state')=="twitter" && request('code'))
     <script>
-setTimeout(function (){
-alert("kk")
-    $.ajax({
-        url:"{{url('social-connect/save-twitter-token')}}",
-        method:'post',
-        data: {"_token": "{{ csrf_token() }}",'code':'{{request('code')}}'},
-        beforeSend:function(){
+          $(document).ready(function (){
+              alert("kk")
+              $.ajax({
+                  url:"{{url('social-connect/save-twitter-token')}}",
+                  method:'post',
+                  data: {"_token": "{{ csrf_token() }}",'code':'{{request('code')}}'},
+                  beforeSend:function(){
 
-        },
-        success:function(response){
+                  },
+                  success:function(response){
 
-            alert("successfull")
-            {{--window.location.href='{{url("social-connect/index")}}'--}}
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
+                      alert("successfull")
+                      {{--window.location.href='{{url("social-connect/index")}}'--}}
+                  },
+                  error: function (jqXHR, textStatus, errorThrown) {
 
-            alert("Unable to connect twitter!")
-        },
-        complete:function(data){
-            alert('{{request('code')}}')
-        }
-    })
-},2000)
+                      alert("Unable to connect twitter!")
+                  },
+                  complete:function(data){
+                      alert('{{request('code')}}')
+                  }
+              })
+          })
     </script>
 @endif
     <script>
