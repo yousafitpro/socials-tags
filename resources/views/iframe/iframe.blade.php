fdbd085d062addebe44bf5eaaca0a1b370d968a2
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-12-04 05:40:30 -0800

    apps icons

--- resources/views/iframe/iframe.blade.php
+++ resources/views/iframe/iframe.blade.php
@@ -7,9 +7,10 @@
     <title>Voters News | Wall</title>
     <link rel="icon" type="image/x-icon" href="{{asset('icon/fabicon.png')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <noscript><meta http-equiv="REFRESH" content="0; url=https://www.mediapass.com/subscription/noscriptredirect?key=7191&asset=7828&uri=https://votersnews.com"></noscript>
+    <noscript><meta http-equiv="REFRESH" content="0; url=https://www.mediapass.com/subscription/noscriptredirect?key=7191&asset=7828&uri=votersnews.com"></noscript>
     <script type="text/javascript" src="https://www.mediapass.com/static/js/mm.js"></script>
     <script type="text/javascript">MediaPass.init(7191, { asset:7828, adBlockOnly: true });</script>
+
 </head>
 {{--//asdasd--}}
 <body>
