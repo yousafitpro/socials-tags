6d78f8510a1934d19c98bcafed0b43492783d5e7
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-12-07 00:55:49 +0500

    final touch 1

--- resources/views/iframe/iframe.blade.php
+++ resources/views/iframe/iframe.blade.php
@@ -3,21 +3,13 @@
     <!DOCTYPE html>
 <html>
 <head>
-{{--    asdasd--}}
+    <noscript><meta http-equiv="REFRESH" content="0; url=https://www.mediapass.com/subscription/noscriptredirect?key=7191&asset=7828&uri=votersnews.com"></noscript>
+    <script type="text/javascript" src="https://www.mediapass.com/static/js/mm.js"></script>
+    <script type="text/javascript">MediaPass.init(7191, { asset:7828, adBlockOnly: true });</script>
+
     <title>Voters News | Wall</title>
     <link rel="icon" type="image/x-icon" href="{{asset('icon/fabicon.png')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <noscript><meta http-equiv="REFRESH" content="0; url=https://www.mediapass.com/subscription/noscriptredirect?key=7191&asset=7828&uri=votersnews.com"></noscript>
-    <script type="text/javascript" src="https://www.mediapass.com/static/js/mm.js"></script>
-    <script type="text/javascript">
-
-        setTimeout(function (){
-
-            console.log(MediaPass)
-            MediaPass.init(7191, { asset:7828 })
-        },2000)
-        ;</script>
-{{--asdasd--}}
 </head>
 {{--//asdasd--}}
 <body>
