3537d15fd8a5262b50242a040eca8862f19a7b3e
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-12-18 14:11:24 +0500

    ok

--- resources/views/iframe/layout.blade.php
+++ resources/views/iframe/layout.blade.php
@@ -9,7 +9,7 @@
     window.OneSignal = window.OneSignal || [];
     OneSignal.push(function() {
         OneSignal.init({
-            appId: "c2c0ca86-dc55-4e00-a582-ee262634d4c3",
+            appId: "53151d56-24d8-4f77-8e29-6da4ffe42aa2",
         });
     });
 </script>
