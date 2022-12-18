36ec9718892e5a5b5e07ff88c3186d9bcf6e2b4f
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-12-18 14:04:44 +0500

    ok

--- resources/views/iframe/layout.blade.php
+++ resources/views/iframe/layout.blade.php
@@ -2,4 +2,14 @@
 
 @include('iframe.css')
 @include('iframe.js')
+
 @yield('content')
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+    window.OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+        OneSignal.init({
+            appId: "c2c0ca86-dc55-4e00-a582-ee262634d4c3",
+        });
+    });
+</script>
