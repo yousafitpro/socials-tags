1c770cebc8bba5435f249cc1d1881afc6f1da41e
Muhammad Yousaf <yousaf.itpro@gmail.com>
2022-12-31 14:15:53 +0500

    get-zpayd

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::group([
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    Route::get('me', 'AuthController@me');
     Route::post('register', 'AuthController@register');
 });
 Route::group([
