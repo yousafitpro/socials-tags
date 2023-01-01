b6a6b48db4dab1e300b0c270867ea42f7f8217c7
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-01-01 10:07:17 +0500

    paypal integration

--- resources/views/paypal/complete_payment.blade.php
+++ resources/views/paypal/complete_payment.blade.php
@@ -40,7 +40,7 @@
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="https://www.paypal.com/sdk/js?client-id=AXGQ83qPEJdEkcuEWlM_LvsEH1S5jc5oCybH4SBGBCLELtX6nUBTQx5AfsBwGji9YuISi1-VBO1dN3Yr&currency=USD&intent=capture&enable-funding=venmo" data-sdk-integration-source="integrationbuilder"></script>
+<script src="https://www.paypal.com/sdk/js?client-id={{config('myconfig.Paypal.client_id')}}&currency=USD&intent=capture&enable-funding=venmo" data-sdk-integration-source="integrationbuilder"></script>
 <script>
 
     var G_amount='{{$amount}}'
