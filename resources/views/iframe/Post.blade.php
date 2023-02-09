c76732c4aff5275ec75444765a57d3bfa3845795
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-02-09 14:21:21 +0500

    adding comments

--- resources/views/iframe/Post.blade.php
+++ resources/views/iframe/Post.blade.php
@@ -53,7 +53,7 @@
 
     <img class="clickOpacity" onclick="showSubscription_Modal()" src="{{asset('wall/soure.png')}}" style="width: 65px">
     @else
-                                  <a href="{{$p->link}}" target="_blank">
+                                  <a href="{{$p->link}}" >
     <img class="clickOpacity" onclick="bookmarkTab()" src="{{asset('wall/soure.png')}}" style="width: 65px">
                                   </a>
     @endif
