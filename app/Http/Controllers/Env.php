01993aebbe2f56848df54b7ce9a551e087c79184
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-02-14 12:08:21 +0500

    first

--- app/Http/Controllers/Env.php
+++ app/Http/Controllers/Env.php
@@ -9,26 +9,7 @@ use Illuminate\Support\Facades\Config;
 
 class Env extends Controller
 {
-    public static function isTestMode()
-    {
-     return true;
-    }
-     public static function  GoogleMap_Key()
-{
-  return Config::get('myconfig.GoogleMap.key');
-}
-    public static function Ligiscan_Key()
-    {
-        return Config::get('myconfig.Ligiscan.key');
-    }
-    public static function Official_Key()
-    {
-        return Config::get('myconfig.Official.key');
-    }
-    public static function Regulatinsgov_Key()
-    {
-        return Config::get('myconfig.Regulatinsgov.key');
-    }
+
 
 
 }
