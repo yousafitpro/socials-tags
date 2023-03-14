f80a4561094311ccd330ec1d0940e8ce1e6b7aa3
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-03-14 20:40:58 +0500

    ok

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Responsive Admin Dashboard Template">
+    <meta name="description" content="Promotamedia | Login">
     <meta name="keywords" content="admin,dashboard">
     <meta name="author" content="stacks">
     <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
