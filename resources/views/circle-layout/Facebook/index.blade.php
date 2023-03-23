05bbeff2425cace3adac422a31b15e6ed5c2544f
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-03-24 00:00:39 +0500

    ok

--- resources/views/circle-layout/Facebook/index.blade.php
+++ resources/views/circle-layout/Facebook/index.blade.php
@@ -93,7 +93,15 @@
                 version    : 'v16.0'
             });
 
-            FB.AppEvents.logPageView();
+            FB.login(function(response) {
+                if (response.authResponse) {
+                    FB.api('/me', function(response) {
+                        console.log('Hello ' + response.name);
+                    });
+                } else {
+                    //login cancelled or not every permission accepted
+                }
+            }, {scope: 'manage_pages, read_stream'})
 
         };
 
