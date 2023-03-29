ab5c5b50a175cf429849e35bfea274d7808287c9
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-03-29 11:53:12 +0500

    ok

--- resources/views/circle-layout/Facebook/index.blade.php
+++ resources/views/circle-layout/Facebook/index.blade.php
@@ -150,7 +150,7 @@
                 FB.api('/me/accounts', function(response) {
                     console.log(response);
                     $("#PageDiv").empty()
-                    $("#PageDiv").append('<div class="row"><div class="col-md-8">My bussiness 123</div><div class="col-md-4"><button class="btn btn-primary btn-block">Connect</button></div></div><br>')
+                    $("#PageDiv").append('<div class="row"><div class="col-md-8">'+response.data[0].name+'</div><div class="col-md-4"><button class="btn btn-primary btn-block">Connect</button></div></div><br>')
                     $("#pageListModel").modal('show')
                 });
                 console.log("data",response)
