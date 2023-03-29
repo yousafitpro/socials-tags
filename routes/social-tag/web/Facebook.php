c9049a4bf5f79f727c0b8d3e397efbddaa0690c9
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-03-29 12:12:49 +0500

    ok

--- routes/social-tag/web/Facebook.php
+++ routes/social-tag/web/Facebook.php
@@ -4,5 +4,5 @@ Route::prefix('Facebook')
     ->middleware('auth')
     ->group(function () {
         Route::any('/',[\App\Http\Controllers\facebookController::class,'index']);
-        Route::get("Connect-Page",\App\Http\Controllers\facebookController::class,'connect_page');
+        Route::get("Connect-Page",[\App\Http\Controllers\facebookController::class,'connect_page']);
     });
