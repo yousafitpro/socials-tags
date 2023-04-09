2a284616bf3173d792d4ab6324613b66fcc4fff0
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-04-09 10:57:28 +0500

    ok

--- resources/views/circle-layout/Facebook/ajax/likes.blade.php
+++ resources/views/circle-layout/Facebook/ajax/likes.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-sm-12">
 {{--                <small style="float: right">{{human_readable_time($p['created_time'])}}</small>--}}
-                <a href="https://web.facebook.com/profile.php?id={{$p['from']['id']}}" target="_blank"><h5>{{$p['from']['name']}}</h5></a>
+                <a href="https://web.facebook.com/profile.php?id={{$p['id']}}" target="_blank"><h5>{{$p['name']}}</h5></a>
 
             </div>
         </div>
