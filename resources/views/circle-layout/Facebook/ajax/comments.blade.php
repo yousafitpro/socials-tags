7138019197f4cc68c39667bf9d66a1910fd2e355
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-04-09 10:41:17 +0500

    ok

--- resources/views/circle-layout/Facebook/ajax/comments.blade.php
+++ resources/views/circle-layout/Facebook/ajax/comments.blade.php
@@ -1,16 +1,21 @@
 
 
 
-@foreach($data as $p)
-    <div class="row">
-        <div class="col-sm-12">
-            <small style="float: right">{{human_readable_time($p['created_time'])}}</small>
-            <h5>{{$p['from']['name']}}</h5>
+@if(count($data)>0)
+    @foreach($data as $p)
+        <div class="row">
+            <div class="col-sm-12">
+                <small style="float: right">{{human_readable_time($p['created_time'])}}</small>
+                <h5>{{$p['from']['name']}}</h5>
 
-            <small>{{$p['message']}}</small>
+                <small>{{$p['message']}}</small>
+            </div>
         </div>
-    </div>
 
-    <br>
-@endforeach
+        <br>
+    @endforeach
+@else
+    <h3 style="text-align: center">No Comments</h3>
+@endif
+
 
