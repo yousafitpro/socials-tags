7cafbee7a868f4ddcb5981ecffacfde99d7c6033
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-04-09 11:21:33 +0500

    ok

--- app/Http/Controllers/facebookController.php
+++ app/Http/Controllers/facebookController.php
@@ -142,6 +142,8 @@ class facebookController extends Controller
     }
     public function post_detail(Request $request,$id)
     {
+        $post=post::find(app_decrypt($id));
+        $id=$post->facebook_post_id;
         $fb=socialConnect::where(['name'=>'Facebook','user_id'=>auth()->id()])->first();
         $url=config('myconfig.FB.ApiUrl').'/'.$id.'/likes?';
         $url=$url.'&access_token='.$fb->page_access_token;
