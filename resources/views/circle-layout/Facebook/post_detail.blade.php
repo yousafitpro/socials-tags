35f2289deb88d27e4cb4190bf453a29a976aff16
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-04-10 14:14:57 +0500

    ok

--- resources/views/circle-layout/Facebook/post_detail.blade.php
+++ resources/views/circle-layout/Facebook/post_detail.blade.php
@@ -28,7 +28,7 @@
                              <a href="https://web.facebook.com/profile.php?id={{$p['from']['id']}}" target="_blank"><h5>{{$p['from']['name']}}</h5></a>
 
 
-                             <small>{{$p['message']}}</small>
+                             <h5>{{$p['message']}}</h5>
                          </div>
                      </div>
 
