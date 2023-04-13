0c10a3ae20c1c21d5fa68fa5e18aee1b87c95f33
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-04-13 16:25:45 +0500

    ok

--- app/Http/Controllers/googleController.php
+++ app/Http/Controllers/googleController.php
@@ -123,7 +123,7 @@ else{
         'user_id'=>auth()->id()
     ]);
 }
-///asdasd
+///asdasdassadasd
 $ac->access_token=$accessToken['access_token'];
 $ac->userid=$userFromGoogle->id;
 $ac->profile_image=$userFromGoogle->picture;
