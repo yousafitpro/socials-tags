b4f374bfeeb5d15e5c527bfa8e2fe25d3204734d
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-04-13 15:51:22 +0500

    ok

--- routes/social-tag/web/google.php
+++ routes/social-tag/web/google.php
@@ -2,7 +2,7 @@
 
 Route::prefix('Google')
     ->group(function () {
-        Route::any('/',[\App\Http\Controllers\googleController::class,'index']);
+        Route::any('/index',[\App\Http\Controllers\googleController::class,'index']);
         Route::any('Login',[\App\Http\Controllers\googleController::class,'login']);
         Route::get('Login-Call-Back',[\App\Http\Controllers\googleController::class,'Login_Call_Back']);
         Route::any('Manage-Business',[\App\Http\Controllers\googleController::class,'manage_business']);
