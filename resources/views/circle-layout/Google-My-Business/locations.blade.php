1abdbedbdb6049b56a49b731d8627d9e95b16c83
Muhammad Yousaf <yousaf.itpro@gmail.com>
2023-04-14 12:14:32 +0500

    ok

--- resources/views/circle-layout/Google-My-Business/locations.blade.php
+++ resources/views/circle-layout/Google-My-Business/locations.blade.php
@@ -1,16 +1,13 @@
 <div class="row">
     <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Locations</h5>
+
 
                 <table id="zero-conf" class="display" style="width:100%">
                     <thead>
                     <tr>
-                        <th>Account Name</th>
-                        <th>Type</th>
-                        <th>Verification State</th>
-                        <th>Vetted State</th>
+                        <th>Name</th>
+                        <th>Title</th>
+
                         <th>Actions</th>
 
                     </tr>
@@ -18,37 +15,28 @@
                     <tbody>
                     @foreach($locations as $l)
 
-{{--                        <tr>--}}
-{{--                            <td>{{$a['accountName']}}</td>--}}
-{{--                            <td>{{$a['type']}}</td>--}}
-{{--                            <td>{{$a['verificationState']}}</td>--}}
-{{--                            <td>{{$a['vettedState']}}</td>--}}
-{{--                            <td>--}}
-{{--                                <div class="dropdown">--}}
-{{--                                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">--}}
-{{--                                    </button>--}}
-{{--                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">--}}
-{{--                                        <li><a class="dropdown-item" onclick="get_locations('{{$loop->index}}','pageListModel{{$loop->index}}','{{url('My-Google/Get-Locations')}}?account_name={{$a['name']}}')" href="#">Locations</a></li>--}}
+                        <tr>
+                            <td>{{$l['name']}}</td>
+                            <td>{{$l['title']}}</td>
+
+                            <td>
+                                <div class="dropdown">
+                                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <li><a class="dropdown-item" onclick="get_locations('{{$loop->index}}','pageListModel{{$loop->index}}','{{url('My-Google/Get-Locations')}}?account_name={{$a['name']}}')" href="#">Locations</a></li>
 
 
-{{--                                    </ul>--}}
-{{--                                </div>--}}
-{{--                            </td>--}}
+                                    </ul>
+                                </div>
+                            </td>
 
-{{--                        </tr>--}}
+                        </tr>
                     @endforeach
 
                     </tbody>
-                    <tfoot>
-                    <tr>
-                        <th>Name</th>
-                        <th>Email / Phone</th>
-                        <th>Platform</th>
-                        <th>date</th>
-                    </tr>
-                    </tfoot>
+
                 </table>
-            </div>
-        </div>
+
     </div>
 </div>
